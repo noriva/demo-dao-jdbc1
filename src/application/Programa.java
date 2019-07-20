f93a7d9e643726c1530cc8b07f54183a74245b5c
noriva <jrmarson@bol.com.br>
2019-07-20 16:25:36 -0300

    Departament entity class

---
+++ src/application/Programa.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Department obj = new Department(1 , "books");
+		System.out.println(obj);
+
+	}
+
+}
