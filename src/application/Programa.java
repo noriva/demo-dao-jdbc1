bf21a08ac02df4494a6791173a3b50281928c603
noriva <jrmarson@bol.com.br>
2019-07-20 18:44:13 -0300

    reusing instantiation

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -12,6 +12,7 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = new DaoFactory().createSellerDao();
+		System.out.println("=== test 1 : seller findbyid====");
 		
 		Seller seller = sellerDao.findById(3);
 		
