751c803f5dd7cea74f46e335903621ac820af82b
noriva <jrmarson@bol.com.br>
2019-07-20 16:58:21 -0300

    Seller dao jdbc and dao factory

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Programa {
 	public static void main(String[] args) {
 		Department obj = new Department(1 , "books");
 		Seller seller = new Seller(21,"bob","bog.com",new Date(),3000.0 , obj);
+		
+		SellerDao sd = DaoFactory.createSellerDao(); 
 		
 		System.out.println(obj);
 		System.out.println(seller);
